82a2c45eeb181b36c5d48b515c0433b6e55cdb37
anoop <freaky4133@gmail.com>
2020-02-06 18:01:05 +0530

    Trending vehicles [Part 1]
    first commit of the trending vehicle section of home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
+import { TrendingComponent } from './home/trending/trending.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    VehicleComponent,
+    TrendingComponent
   ],
   imports: [
     BrowserModule,
